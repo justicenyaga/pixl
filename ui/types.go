144b3749f2c322ac66d547eb240b16e8630ebb06
Justice Nyaga <justiceevansmwema@gmail.com>
2024-02-08 21:06:51 +0300

    Pixel Canvas Layout

--- ui/types.go
+++ ui/types.go
@@ -3,10 +3,12 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"justicenyaga.io/pixl/apptype"
+	"justicenyaga.io/pixl/pxcanvas"
 	"justicenyaga.io/pixl/swatch"
 )
 
 type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
 	PixlWindow fyne.Window
 	State      *apptype.State
 	Swatches   []*swatch.Swatch
